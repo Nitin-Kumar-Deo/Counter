d54765da27633276fd6b871138d409f7830c2e6f
nitin <nitindeo9r@gmail.com>
2022-07-06 23:27:44 +0530

    Updates

---
+++ precache-manifest.bed46145b2915afe88b2d4f39a72cb32.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78046cf490936343fd04202ca6fbe3d9",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "c0e544843cfa412027e8",
+    "url": "/Counter/static/js/2.97bd378f.chunk.js"
+  },
+  {
+    "revision": "b623d71a5771e7b15559",
+    "url": "/Counter/static/js/main.aa61f775.chunk.js"
+  },
+  {
+    "revision": "1c8b15eed4b5e3692c1e",
+    "url": "/Counter/static/js/runtime-main.b6c4a3a3.js"
+  }
+]);
